d6aa31bff867f48bae55b2b96513a9fdd4cf8be6
yann tyb <yanntyb.lbc@gmail.com>
2021-10-06 09:02:53 +0200

    create publications css

--- View/Article/create.view.php
+++ View/Article/create.view.php
@@ -1,3 +1,31 @@
+<link rel="stylesheet" href="View/assets/create.css">
 <div id="main">
-
+    <div>
+        <form action="">
+            <div>
+                <h2>Titre :</h2>
+                <input name="title" type="text">
+            </div>
+            <div>
+                <h2>Contenu :</h2>
+                <textarea name="content"></textarea>
+            </div>
+            <div>
+                <h2>Categorie</h2>
+                <div class="select">
+                    <select name="category">
+                        <?php
+                        foreach($var as $cat){?>
+                            <option value="<?= $cat->getId()?>"><?= $cat->getName() ?></option>
+                            <?php
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <div id="submit">
+                <input type="submit" value="Publier">
+            </div>
+        </form>
+    </div>
 </div>
